aafc7311a4e75e0ef80fa760ab2c17a1e71830fe
hoodlm <hoodlma@gmail.com>
2014-02-21 18:21:21 -0500

    Added JIJNA2 template; rendering main.html

--- src/main.py
+++ src/main.py
@@ -14,12 +14,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
+import os
+import urllib
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import jinja2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = JINJA_ENVIRONMENT.get_template('main.html')
+        self.response.write(template.render({}))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
