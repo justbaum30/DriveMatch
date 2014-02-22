1fa58dea5b24a44c1cd5a4ba8d60cb98af324df5
hoodlm <hoodlma@gmail.com>
2014-02-21 21:16:02 -0500

    expanded model properties

--- src/model.py
+++ src/model.py
@@ -1,23 +1,37 @@
 from google.appengine.ext import ndb
 
+class Account(ndb.Model):
+    user = ndb.UserProperty()
+    hostedEvents = ndb.StructuredProperty(Event, repeated = True)
+    attendedEvents = ndb.StructuredProperty(Event, repeated = True)
 
 class Guest(ndb.Model):
-    user = ndb.UserProperty()
+    account = ndb.StructuredProperty(Account)
     nickname = ndb.StringProperty()
-
-class Driver(Guest):
+    canDrive = ndb.BooleanProperty()
+    isDriving = ndb.BooleanProperty()
     seats = ndb.IntegerProperty()
-
-class Passenger(Guest):
-    age = ndb.IntegerProperty()
+    milesPerGallon = ndb.FloatProperty()
 
 class Carpool(ndb.Model):
-    key = ndb.KeyProperty()
-    driver = ndb.StructuredProperty(Driver)
-    passengers = ndb.StructuredProperty(Passenger, repeated = True)
+    name = ndb.StringProperty()
+    driver = ndb.StructuredProperty(Guest)
+    passengers = ndb.StructuredProperty(Guest, repeated = True)
+    
+    departureTime = ndb.DateTimeProperty()
+    departureLocation = nbd.StringProperty()
+    returnTime = ndb.DateTimeProperty()
+    returnLocation = nbd.StringProperty()
 
 class Event(ndb.Model):
-    key = ndb.KeyProperty()
-    eventTimeAndDate = ndb.DateTimeProperty()
+    name = ndb.StringProperty()
+    eventLocation = nbd.StringProperty()
+
+    departureTime = ndb.DateTimeProperty()
+    departureLocation = nbd.StringProperty()
+    returnTime = ndb.DateTimeProperty()
+    returnLocation = nbd.StringProperty()
+
     host = ndb.StructuredProperty(Guest)
     guests = ndb.StructuredProperty(Guest, repeated = True)
+    carpools = ndb.StructuredProperty(Carpool, repeated = True)
