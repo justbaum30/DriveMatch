3fdf9762789a7af83185e7af4ede779c9bc09334
Justin Baumgartner <justbaum30@gmail.com>
2014-02-22 04:37:00 -0500

    Can now import a CSV.

--- src/assets/scripts/create.js
+++ src/assets/scripts/create.js
@@ -5,7 +5,8 @@ $(function() {
     	minuteStep: 15,
     	showMeridian: true
 	});
-
+	$('#guestTable').hide();
+	$('#upload-progress').hide();
 
 	$('#createSubmitButton').on('click', function() {
 		$.ajax({
@@ -20,72 +21,24 @@ $(function() {
 				guests: []
 			},
 			success: function() { 
-				console.log("Submitted dat form"); 
+				console.log("success!"); 
 			},
 			error: function(jqXHR, textStatus, errorThrown) { 
-				console.error("Shit went wrong."); 
+				console.error('Failure!'); 
 			}
-			success: function() { console.log("success!"); },
-			error: function(jqXHR, textStatus, errorThrown) { console.error('Failure!'); }
 		});
 	});
-});
-
-// Update the file upload progress bar
-function updateProgress(newValue){
-  $('.progress-bar').css('width', newValue+'%').attr('aria-valuenow', newValue); 
-}
-
-// Google places autocomplete
-var startLocation = document.getElementById('eventLocation');
-var autocompleteStart = new google.maps.places.Autocomplete(startLocation);
-
-var endLocation = document.getElementById('departureLocation');
-var autocompleteEnd = new google.maps.places.Autocomplete(endLocation);
-
-// dummy user data
-var userData = [{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"}];
-
-// Generate DataTable
-function initializeTable(data){
-
-	// editor = new $.fn.dataTable.Editor( {
- //        "ajaxUrl": "php/browsers.php",
- //        "domTable": "#dataTable",
- //        "fields": [ {
- //                "label": "Browser:",
- //                "name": "browser"
- //            }, {
- //                "label": "Rendering engine:",
- //                "name": "engine"
- //            }
- //        ]
- //    } );
-
-  $('#guestTable').hide();
-  var myColumns = [
-        	{ "mDataProp": "personName" },
-        	{ "mDataProp": "personEmail" },
-    	];
-  var table = $('#dataTable').dataTable({
-      "aaData": data,
-      "aoColumns": [
-          { "mDataProp": "personName" },
-          { "mDataProp": "personEmail" }
-      ]
-  });
-  $('#guestTable').fadeIn('slow');
-}
-
-initializeTable(userData);
 
 	$('#contactFileInput').on('change', function() {
+		$('#upload-progress').show();
+		updateProgress(0);
 		var file = this.files[0];
 		var reader = new FileReader();
 		reader.readAsText(file);
 		reader.onload = function(event) {
 			var csv = event.target.result;
 			var headers = $.csv.toArrays(csv)[0];
+			updateProgress(50);
 			var nameIndex;
 			var emailIndex;
 			for (var i=0; i<headers.length; i++) {
@@ -104,16 +57,49 @@ initializeTable(userData);
 				var tableItem = new Object();
 				for (var propName in object) {
 					var propValue = object[propName];
-					if (propName.toLowerCase().indexOf("name") != -1 && propValue != '' && tableItem.name == undefined)
-						tableItem.name = propValue;
-					if (propName.toLowerCase().indexOf("e-mail") != -1 && propValue != '' && propValue.indexOf('@') != -1 && tableItem.email == undefined)
-						tableItem.email = propValue;
+					if (propName.toLowerCase().indexOf("name") != -1 && propValue != '' && tableItem.personName == undefined)
+						tableItem.personName = propValue;
+					if (propName.toLowerCase().indexOf("e-mail") != -1 && propValue != '' && propValue.indexOf('@') != -1 && tableItem.personEmail == undefined)
+						tableItem.personEmail = propValue;
 				}
-				if (tableItem.name != undefined && tableItem.email != undefined) {
+				if (tableItem.personName != undefined && tableItem.personEmail != undefined) {
 					tableData.push(tableItem);
 				}
 			}
-			console.log(tableData);
+			updateProgress(100);
+			$('#upload-progress').hide();
+			initializeTable(tableData);
 		}
 	});
+
+	// Update the file upload progress bar
+	function updateProgress(newValue){
+	  $('.progress-bar').css('width', newValue+'%').attr('aria-valuenow', newValue); 
+	}
+
+	// Google places autocomplete
+	var startLocation = document.getElementById('eventLocation');
+	var autocompleteStart = new google.maps.places.Autocomplete(startLocation);
+
+	var endLocation = document.getElementById('departureLocation');
+	var autocompleteEnd = new google.maps.places.Autocomplete(endLocation);
+
+	// dummy user data
+	var userData = [{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"},{"personName":"Jacob","personEmail":"bsabugler@gmail.com"}];
+
+	// Generate DataTable
+	function initializeTable(data){
+	  var myColumns = [
+	        	{ "mDataProp": "personName" },
+	        	{ "mDataProp": "personEmail" },
+	    	];
+	  var table = $('#dataTable').dataTable({
+	      "aaData": data,
+	      "aoColumns": [
+	          { "mDataProp": "personName" },
+	          { "mDataProp": "personEmail" }
+	      ]
+	  });
+	  $('#guestTable').show();
+	}
 });
